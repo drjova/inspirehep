bf05b18284fb037b5b86ddf3c8d57b2e4dd28a15
Harris Tzovanakis <me@drjova.com>
2019-01-16 14:44:17 +0100

    more working stuff

--- inspirehep/config.py
+++ inspirehep/config.py
@@ -20,23 +20,21 @@ from datetime import timedelta
 
 from invenio_indexer.api import RecordIndexer
 from invenio_records_rest.facets import terms_filter
-from invenio_records_rest.utils import allow_all, check_elasticsearch
+from invenio_records_rest.utils import allow_all, check_elasticsearch, deny_all
 from invenio_search import RecordsSearch
-
 
 def _(x):
     """Identity function used to trigger string extraction."""
     return x
 
-
 # DEBUG
-FLASK_ENV = "development"
+FLASK_ENV = 'development'
 FLASK_DEBUG = 1
 
 # Rate limiting
 # =============
 #: Storage for ratelimiter.
-RATELIMIT_STORAGE_URL = "redis://localhost:6379/3"
+RATELIMIT_STORAGE_URL = 'redis://localhost:6379/3'
 
 # Email configuration
 # ===================
@@ -50,36 +48,36 @@ MAIL_SUPPRESS_SEND = True
 #: Email address used as sender of account registration emails.
 SECURITY_EMAIL_SENDER = SUPPORT_EMAIL
 #: Email subject for account registration emails.
-SECURITY_EMAIL_SUBJECT_REGISTER = _("Welcome to inspirehep!")
+SECURITY_EMAIL_SUBJECT_REGISTER = _(
+    "Welcome to inspirehep!")
 #: Redis session storage URL.
-ACCOUNTS_SESSION_REDIS_URL = "redis://localhost:6379/1"
+ACCOUNTS_SESSION_REDIS_URL = 'redis://localhost:6379/1'
 
 # Celery configuration
 # ====================
 
-BROKER_URL = "amqp://guest:guest@localhost:5672/"
+BROKER_URL = 'amqp://guest:guest@localhost:5672/'
 #: URL of message broker for Celery (default is RabbitMQ).
-CELERY_BROKER_URL = "amqp://guest:guest@localhost:5672/"
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672/'
 #: URL of backend for result storage (default is Redis).
-CELERY_RESULT_BACKEND = "redis://localhost:6379/2"
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/2'
 #: Scheduled tasks configuration (aka cronjobs).
 CELERY_BEAT_SCHEDULE = {
-    "indexer": {
-        "task": "invenio_indexer.tasks.process_bulk_queue",
-        "schedule": timedelta(minutes=5),
-    },
-    "accounts": {
-        "task": "invenio_accounts.tasks.clean_session_table",
-        "schedule": timedelta(minutes=60),
-    },
+    #'indexer': {
+    #    'task': 'invenio_indexer.tasks.process_bulk_queue',
+    #    'schedule': timedelta(minutes=5),
+    #},
+    #'accounts': {
+    #    'task': 'invenio_accounts.tasks.clean_session_table',
+    #    'schedule': timedelta(minutes=60),
+    #},
 }
 
 # Database
 # ========
 #: Database URI including user and password
-SQLALCHEMY_DATABASE_URI = (
-    "postgresql+psycopg2://inspirehep:inspirehep@localhost/inspirehep"
-)
+SQLALCHEMY_DATABASE_URI = \
+    'postgresql+psycopg2://inspirehep:inspirehep@localhost/inspirehep'
 
 # JSONSchemas
 # ===========
@@ -93,7 +91,7 @@ JSONSCHEMAS_HOST = "localhost:5000"
 
 #: Secret key - each installation (dev, production, ...) needs a separate key.
 #: It should be changed before deploying.
-SECRET_KEY = "CHANGE_ME"
+SECRET_KEY = 'CHANGE_ME'
 #: Max upload size for form data via application/mulitpart-formdata.
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MiB
 #: Sets cookie with the secure flag by default
@@ -102,11 +100,11 @@ SESSION_COOKIE_SECURE = True
 #: provided, the allowed hosts variable is set to localhost. In production it
 #: should be set to the correct host and it is strongly recommended to only
 #: route correct hosts to the application.
-APP_ALLOWED_HOSTS = ["inspirehep.net", "localhost", "127.0.0.1"]
+APP_ALLOWED_HOSTS = ['inspirehep.net', 'localhost', '127.0.0.1']
 
 # OAI-PMH
 # =======
-OAISERVER_ID_PREFIX = "oai:inspirehep.net:"
+OAISERVER_ID_PREFIX = 'oai:inspirehep.net:'
 
 # Debug
 # =====
@@ -119,45 +117,53 @@ DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 RECORDS_REST_ENDPOINTS = {
-    "recid": dict(
-        pid_type="recid",
-        pid_minter="recid",
-        pid_fetcher="recid",
+    'recid': dict(
+        pid_type='recid',
+        pid_minter='recid',
+        pid_fetcher='recid',
         default_endpoint_prefix=True,
         search_class=RecordsSearch,
         indexer_class=RecordIndexer,
-        search_index="records",
+        search_index='records',
         search_type=None,
         record_serializers={
-            "application/json": ("inspirehep.records.serializers" ":json_v1_response")
+            'application/json': ('inspirehep.records.serializers'
+                                 ':json_v1_response'),
         },
         search_serializers={
-            "application/json": ("inspirehep.records.serializers" ":json_v1_search")
+            'application/json': ('inspirehep.records.serializers'
+                                 ':json_v1_search'),
         },
-        record_loaders={"application/json": ("inspirehep.records.loaders" ":json_v1")},
-        list_route="/records/",
-        item_route="/records/<pid(recid):pid_value>",
-        default_media_type="application/json",
+        record_loaders={
+            'application/json': ('inspirehep.records.loaders'
+                                 ':json_v1'),
+        },
+        list_route='/records/',
+        item_route='/records/<pid(recid):pid_value>',
+        default_media_type='application/json',
         max_result_window=10000,
         error_handlers=dict(),
         create_permission_factory_imp=allow_all,
         read_permission_factory_imp=check_elasticsearch,
-        update_permission_factory_imp=allow_all,
-        delete_permission_factory_imp=allow_all,
-        list_permission_factory_imp=allow_all,
-    )
+        update_permission_factory_imp=deny_all,
+        delete_permission_factory_imp=deny_all,
+        list_permission_factory_imp=allow_all
+    ),
 }
 """REST API for inspirehep."""
 
-PIDSTORE_RECID_FIELD = "control_number"
+PIDSTORE_RECID_FIELD = 'control_number'
 
 RECORDS_REST_FACETS = dict(
     records=dict(
         aggs=dict(
-            type=dict(terms=dict(field="type")),
-            keywords=dict(terms=dict(field="keywords")),
+            type=dict(terms=dict(field='type')),
+            keywords=dict(terms=dict(field='keywords'))
         ),
-        post_filters=dict(type=terms_filter("type"), keywords=terms_filter("keywords")),
+        post_filters=dict(
+            type=terms_filter('type'),
+            keywords=terms_filter('keywords'),
+        )
     )
 )
 """Introduce searching facets."""
@@ -166,15 +172,26 @@ RECORDS_REST_FACETS = dict(
 RECORDS_REST_SORT_OPTIONS = dict(
     records=dict(
         bestmatch=dict(
-            title=_("Best match"), fields=["_score"], default_order="desc", order=1
+            title=_('Best match'),
+            fields=['_score'],
+            default_order='desc',
+            order=1,
         ),
         mostrecent=dict(
-            title=_("Most recent"), fields=["-_created"], default_order="asc", order=2
+            title=_('Most recent'),
+            fields=['-_created'],
+            default_order='asc',
+            order=2,
         ),
     )
 )
 """Setup sorting options."""
 
 
-RECORDS_REST_DEFAULT_SORT = dict(records=dict(query="bestmatch", noquery="mostrecent"))
+RECORDS_REST_DEFAULT_SORT = dict(
+    records=dict(
+        query='bestmatch',
+        noquery='mostrecent',
+    )
+)
 """Set default sorting options."""
