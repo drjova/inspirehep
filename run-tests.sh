6b4dfe0fe71ae4e42a1e7234c4d35c69d17dabb0
Harris Tzovanakis <me@drjova.com>
2019-01-17 22:55:37 +0100

    separate tests

--- run-tests.sh
+++ run-tests.sh
@@ -9,4 +9,5 @@
 pipenv check --ignore 36437 --ignore 36759 && \
 pipenv run isort -rc -c -df && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
-pipenv run py.test tests/
+pipenv run py.test tests/unit
+pipenv run py.test tests/integration
