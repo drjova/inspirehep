d38673ef9a76e30b907ac611ebd0516cf3cc22f2
Harris Tzovanakis <me@drjova.com>
2021-03-24 11:39:29 +0100

    ui: fix success alert for assigning papers to conferences

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 const localProxy = proxy({
-  target: 'http://localhost:8000',
+  target: 'https://inspirebeta.net',
   secure: false,
   changeOrigin: true,
 });
